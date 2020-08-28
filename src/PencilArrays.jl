6e94072354473dd79e19c07dc2dbc2246023bec5
Juan Ignacio Polanco <jipolanc@gmail.com>
2020-08-28 11:34:35 +0200

    Copy source files from PencilFFTs.jl

--- src/PencilArrays.jl
+++ src/PencilArrays.jl
@@ -1,5 +1,39 @@
+"""
+    PencilArrays
+
+Array wrappers for MPI-distributed data.
+"""
 module PencilArrays
 
-# Write your package code here.
+using MPI
+using OffsetArrays
+using Reexport
+using TimerOutputs
+
+import Base: @propagate_inbounds
+import LinearAlgebra
+
+include("Permutations/Permutations.jl")
+using .Permutations
+
+include("Pencils/Pencils.jl")
+@reexport using .Pencils
+import .Pencils:
+    get_comm, get_permutation, range_local, size_local, size_global
+
+export PencilArray, PencilArrayCollection, ManyPencilArray
+export pencil
+export gather
+export global_view
+export ndims_extra, ndims_space, extra_dims
+
+# Type definitions
+include("arrays.jl")       # PencilArray
+include("multiarrays.jl")  # ManyPencilArray
+include("global_view.jl")  # GlobalPencilArray
+include("cartesian_indices.jl")  # PermutedLinearIndices, PermutedCartesianIndices
+
+include("Transpositions/Transpositions.jl")
+export Transpositions
 
 end
