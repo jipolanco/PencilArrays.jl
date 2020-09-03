451faa4a24a9fefa04bc70ad5059957a0690c244
Juan Ignacio Polanco <jipolanc@gmail.com>
2020-09-03 18:52:43 +0200

    Build docs on MPI
    This should make it possible to run examples, including Literate files,
    when building the docs.

--- docs/make.jl
+++ docs/make.jl
@@ -1,39 +1,56 @@
+using MPI
 using HDF5  # to load HDF5 code via Requires
 using PencilArrays
 using Documenter
 
-DocMeta.setdocmeta!(PencilArrays.Permutations, :DocTestSetup,
-                    :(using PencilArrays.Permutations); recursive=true)
+function main()
+    rank = MPI.Comm_rank(MPI.COMM_WORLD)
+    keepdocs = rank == 0  # the docs from this process will be kept and deployed
 
-makedocs(;
-    modules = [PencilArrays],
-    authors = "Juan Ignacio Polanco <jipolanc@gmail.com> and contributors",
-    repo = "https://github.com/jipolanco/PencilArrays.jl/blob/{commit}{path}#L{line}",
-    sitename = "PencilArrays.jl",
-    format = Documenter.HTML(;
-        prettyurls = true,
-        canonical = "https://jipolanco.github.io/PencilArrays.jl",
-        assets = [
-            "assets/custom.css",
-            "assets/matomo.js",
-        ],
-    ),
-    pages = [
-        "Home" => "index.md",
-        "Library" => [
-            "MPITopology.md",
-            "Pencils.md",
-            "PencilArrays.md",
-            "Transpositions.md",
-            "PencilIO.md",
-            "PencilArrays_timers.md",
-            "Internals" => ["PermutationUtils.md"]
-        ],
-    ],
-    linkcheck = true,
-)
+    DocMeta.setdocmeta!(PencilArrays.Permutations, :DocTestSetup,
+                        :(using PencilArrays.Permutations); recursive=true)
 
-deploydocs(;
-    repo = "github.com/jipolanco/PencilArrays.jl",
-    forcepush = true,
-)
+    mktempdir() do tempdir
+        makedocs(;
+            modules = [PencilArrays],
+            authors = "Juan Ignacio Polanco <jipolanc@gmail.com> and contributors",
+            repo = "https://github.com/jipolanco/PencilArrays.jl/blob/{commit}{path}#L{line}",
+            sitename = "PencilArrays.jl",
+            build = keepdocs ? "build" : tempdir,
+            format = Documenter.HTML(;
+                prettyurls = true,
+                canonical = "https://jipolanco.github.io/PencilArrays.jl",
+                assets = [
+                    "assets/custom.css",
+                    "assets/matomo.js",
+                ],
+            ),
+            pages = [
+                "Home" => "index.md",
+                "Library" => [
+                    "MPITopology.md",
+                    "Pencils.md",
+                    "PencilArrays.md",
+                    "Transpositions.md",
+                    "PencilIO.md",
+                    "PencilArrays_timers.md",
+                    "Internals" => ["PermutationUtils.md"]
+                ],
+            ],
+            linkcheck = keepdocs,
+        )
+    end
+
+    if keepdocs
+        deploydocs(;
+            repo = "github.com/jipolanco/PencilArrays.jl",
+            forcepush = true,
+        )
+    end
+
+    nothing
+end
+
+MPI.Initialized() || MPI.Init()
+
+main()
