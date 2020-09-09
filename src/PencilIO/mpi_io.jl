f72c0b85f6becc4a3dff2e71114325ffe1cfbb77
Juan Ignacio Polanco <jipolanc@gmail.com>
2020-09-09 22:07:53 +0200

    Add reading functions and support offsets

--- src/PencilIO/mpi_io.jl
+++ src/PencilIO/mpi_io.jl
@@ -26,12 +26,18 @@ Base.open(D::MPIIODriver, filename::AbstractString, comm::MPI.Comm; keywords...)
     )
 
 """
-    write(ff::MPI.FileHandle, x::PencilArray;
-          chunks=false, collective=true, infokws...)
+    write(file::MPI.FileHandle, x::PencilArray; offset=0, chunks=false,
+          collective=true, infokws...) -> Int
 
 Write [`PencilArray`](@ref) to binary file using MPI-IO.
 
+Returns the number of bytes written by all processes.
+This may be useful for setting the `offset` argument in a future call to this
+function.
+
 # Optional arguments
+
+- `offset` is the number of bytes to skip from the beginning of the file.
 
 - if `chunks = true`, data is written in contiguous blocks, with one block per
   process.
@@ -44,27 +50,46 @@ Write [`PencilArray`](@ref) to binary file using MPI-IO.
   usually recommended for performance.
 
 - when writing discontiguous blocks, additional keyword arguments are passed via
-  a `MPI.Info` object to `MPI.File.set_view!`. This is ignored if `chunks = true`.
+  an `MPI.Info` object to `MPI.File.set_view!`. This is ignored if `chunks = true`.
 
 """
 function Base.write(ff::MPI.FileHandle, x::PencilArray;
-                    collective=true, chunks=false, kw...)
+                    offset=0, collective=true, chunks=false, kw...)
     if chunks
-        write_contiguous(ff, x; collective=collective, kw...)
+        write_contiguous(ff, x; offset=offset, collective=collective, kw...)
     else
-        write_discontiguous(ff, x; collective=collective, kw...)
+        write_discontiguous(ff, x; offset=offset, collective=collective, kw...)
     end
-    nothing
+    prod(size_global(x)) * sizeof(eltype(x))
+end
+
+"""
+    read!(file::MPI.FileHandle, x::PencilArray; offset=0, chunks=false,
+          collective=true, infokws...) -> Int
+
+Read binary data from an MPI-IO stream, filling in [`PencilArray`](@ref).
+
+Returns the number of bytes written by all processes.
+This may be useful for setting the `offset` argument in a future call to this
+function.
+
+See [`write`](@ref write(::MPI.FileHandle)) for details on keyword arguments.
+"""
+function Base.read!(ff::MPI.FileHandle, x::PencilArray;
+                    offset=0, collective=true, chunks=false, kw...)
+    if chunks
+        read_contiguous!(ff, x; offset=offset, collective=collective, kw...)
+    else
+        read_discontiguous!(ff, x; offset=offset, collective=collective, kw...)
+    end
+    prod(size_global(x)) * sizeof(eltype(x))
 end
 
 function write_discontiguous(ff::MPI.FileHandle, x::PencilArray;
-                             collective, infokws...)
+                             offset, collective, infokws...)
     to = get_timer(pencil(x))
     @timeit_debug to "Write MPI-IO discontiguous" begin
-        disp = 0
-        etype = MPI.Datatype(eltype(x))
-        filetype = create_discontiguous_datatype(x, MemoryOrder())  # TODO cache datatype?
-        MPI.File.set_view!(ff, 0, etype, filetype; infokws...)
+        set_view!(ff, x, offset; infokws...)
         A = parent(x)
         if collective
             MPI_File_write_all(ff, A)
@@ -73,6 +98,20 @@ function write_discontiguous(ff::MPI.FileHandle, x::PencilArray;
         end
     end
     nothing
+end
+
+function read_discontiguous!(ff::MPI.FileHandle, x::PencilArray;
+                             offset, collective, infokws...)
+    to = get_timer(pencil(x))
+    @timeit_debug to "Read MPI-IO discontiguous" begin
+        set_view!(ff, x, offset; infokws...)
+        A = parent(x)
+        if collective
+            MPI_File_read_all!(ff, A)
+        else
+            MPI_File_read!(ff, A)
+        end
+    end
 end
 
 # TODO add these to MPI.jl
@@ -96,10 +135,38 @@ function MPI_File_write_all(file::MPI.FileHandle, buf::MPI.Buffer)
     return stat_ref[]
 end
 
-MPI_File_write(file::MPI.FileHandle, data) = MPI_File_write(file, MPI.Buffer_send(data))
-MPI_File_write_all(file::MPI.FileHandle, data) = MPI_File_write_all(file, MPI.Buffer_send(data))
+function MPI_File_read!(file::MPI.FileHandle, buf::MPI.Buffer)
+    stat_ref = Ref{MPI.Status}(MPI.STATUS_EMPTY)
+    # int MPI_File_read(MPI_File fh, void *buf,
+    #                   int count, MPI_Datatype datatype, MPI_Status *status)
+    MPI.@mpichk ccall((:MPI_File_read, MPI.libmpi), Cint,
+                      (MPI.MPI_File, MPI.MPIPtr, Cint, MPI.MPI_Datatype, Ptr{MPI.Status}),
+                      file, buf.data, buf.count, buf.datatype, stat_ref)
+    return stat_ref[]
+end
 
-function create_discontiguous_datatype(x::PencilArray, order=MemoryOrder())
+function MPI_File_read_all!(file::MPI.FileHandle, buf::MPI.Buffer)
+    stat_ref = Ref{MPI.Status}(MPI.STATUS_EMPTY)
+    # int MPI_File_read_all(MPI_File fh, void *buf,
+    #                       int count, MPI_Datatype datatype, MPI_Status *status)
+    MPI.@mpichk ccall((:MPI_File_read_all, MPI.libmpi), Cint,
+                      (MPI.MPI_File, MPI.MPIPtr, Cint, MPI.MPI_Datatype, Ptr{MPI.Status}),
+                      file, buf.data, buf.count, buf.datatype, stat_ref)
+    return stat_ref[]
+end
+
+for f in (:MPI_File_write, :MPI_File_write_all, :MPI_File_read!, :MPI_File_read_all!)
+    @eval $f(file::MPI.FileHandle, data) = $f(file, MPI.Buffer_send(data))
+end
+
+function set_view!(ff, x::PencilArray, offset; infokws...)
+    etype = MPI.Datatype(eltype(x))
+    filetype = create_discontiguous_datatype(x, MemoryOrder())  # TODO cache datatype?
+    MPI.File.set_view!(ff, offset, etype, filetype; infokws...)
+    nothing
+end
+
+function create_discontiguous_datatype(x::PencilArray, order)
     sizes = size_global(x, order)
     subsizes = size_local(x, order)
     offsets = map(r -> first(r) - 1, range_local(x, order))
@@ -110,15 +177,30 @@ function create_discontiguous_datatype(x::PencilArray, order=MemoryOrder())
 end
 
 function write_contiguous(ff::MPI.FileHandle, x::PencilArray;
-                          collective, infokws...)
+                          offset, collective, infokws...)
     to = get_timer(pencil(x))
     @timeit_debug to "Write MPI-IO contiguous" begin
-        offset = mpi_io_offset(x)
+        offset += mpi_io_offset(x)
         A = parent(x)
         if collective
             MPI.File.write_at_all(ff, offset, A)
         else
             MPI.File.write_at(ff, offset, A)
+        end
+    end
+    nothing
+end
+
+function read_contiguous!(ff::MPI.FileHandle, x::PencilArray;
+                          offset, collective, infokws...)
+    to = get_timer(pencil(x))
+    @timeit_debug to "Read MPI-IO contiguous" begin
+        offset += mpi_io_offset(x)
+        A = parent(x)
+        if collective
+            MPI.File.read_at_all!(ff, offset, A)
+        else
+            MPI.File.read_at!(ff, offset, A)
         end
     end
     nothing
