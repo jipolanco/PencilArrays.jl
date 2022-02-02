d7328779b4ffd638b728cface3c39daebfdf00eb
Juan Ignacio Polanco <jipolanc@gmail.com>
2022-02-02 09:23:35 +0100

    Add binary broadcasting rules for PencilArray
    This is to make sure that A::PencilArray .+ B::OtherKindOfArray returns
    a PencilArray, which wasn't previously the case when B was a GPU array...

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -1,42 +1,56 @@
-using Base.Broadcast: BroadcastStyle, ArrayStyle, DefaultArrayStyle, Broadcasted
+using Base.Broadcast:
+    BroadcastStyle, Broadcasted, AbstractArrayStyle, DefaultArrayStyle
 
-for PA in (PencilArray, GlobalPencilArray)
-    @eval begin
-        BroadcastStyle(::Type{<:$PA}) = ArrayStyle{$PA}()
+abstract type AbstractPencilArrayStyle{N} <: AbstractArrayStyle{N} end
 
-        function Base.similar(bc::Broadcasted{ArrayStyle{$PA}}, ::Type{T}) where {T}
-            A = find_pa(bc)
-            if axes(bc) != axes(A)
-                throw(DimensionMismatch("arrays cannot be broadcast; got axes $(axes(bc)) and $(axes(A))"))
-            end
-            similar(A, T)
-        end
+struct PencilArrayStyle{N} <: AbstractPencilArrayStyle{N} end
+struct GlobalPencilArrayStyle{N} <: AbstractPencilArrayStyle{N} end
 
-        find_pa(A::$PA, rest) = A
+BroadcastStyle(::Type{<:PencilArray{T, N}}) where {T, N} =
+    PencilArrayStyle{N}()
+
+# PencilArrayStyle wins against other array styles
+BroadcastStyle(style::AbstractPencilArrayStyle, ::AbstractArrayStyle) = style
+
+# Needed to avoid ambiguities
+BroadcastStyle(
+    style::AbstractPencilArrayStyle{N}, ::DefaultArrayStyle{N},
+) where {N} = style
+
+BroadcastStyle(::Type{<:GlobalPencilArray{T, N}}) where {T, N} =
+    GlobalPencilArrayStyle{N}()
+
+function Base.similar(
+        bc::Broadcasted{<:AbstractPencilArrayStyle}, ::Type{T},
+    ) where {T}
+    A = find_pa(bc)
+    if axes(bc) != axes(A)
+        throw(DimensionMismatch("arrays cannot be broadcast; got axes $(axes(bc)) and $(axes(A))"))
     end
+    similar(A, T)
 end
-
-# Make PencilArray and GlobalPencilArray incompatible for broadcasting.
-# Without this, broadcasting will work with 1 MPI process, but fail with more
-# (with an error by OffsetArrays), which is annoying when testing code.
-function BroadcastStyle(
-        ::ArrayStyle{GlobalPencilArray},
-        ::ArrayStyle{A},
-    ) where {A <: AbstractArray}
-    throw(ArgumentError("cannot combine $A and GlobalPencilArray in broadcast"))
-end
-
-# For the same reasons, disallow broadcasting between generic array and
-# GlobalPencilArray.
-function BroadcastStyle(::ArrayStyle{GlobalPencilArray}, ::DefaultArrayStyle)
-    throw(ArgumentError("cannot combine generic arrays and GlobalPencilArray in broadcast"))
-end
-
-# Exception: broadcasting with scalars.
-BroadcastStyle(style::ArrayStyle{GlobalPencilArray}, ::DefaultArrayStyle{0}) = style
 
 # Find PencilArray or GlobalPencilArray among broadcast arguments.
 find_pa(bc::Broadcasted) = find_pa(bc.args)
 find_pa(args::Tuple) = find_pa(find_pa(args[1]), Base.tail(args))
 find_pa(x) = x
 find_pa(::Any, rest) = find_pa(rest)
+find_pa(A::Union{PencilArray, GlobalPencilArray}, rest) = A
+
+# Make PencilArray and GlobalPencilArray incompatible for broadcasting.
+# Without this, broadcasting will work with 1 MPI process, but fail with more
+# (with an error by OffsetArrays), which is annoying when testing code.
+function BroadcastStyle(::GlobalPencilArrayStyle, ::PencilArrayStyle)
+    throw(ArgumentError(
+        "cannot combine PencilArray and GlobalPencilArray in broadcast"
+    ))
+end
+
+# For the same reasons, disallow broadcasting between generic array and
+# GlobalPencilArray.
+function BroadcastStyle(::GlobalPencilArrayStyle, ::AbstractArrayStyle)
+    throw(ArgumentError("cannot combine generic arrays and GlobalPencilArray in broadcast"))
+end
+
+# Exception: broadcasting with scalars.
+BroadcastStyle(style::GlobalPencilArrayStyle, ::AbstractArrayStyle{0}) = style
