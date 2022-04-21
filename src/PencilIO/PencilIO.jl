b14c01f1ff5c381825a83532e02c8bed54673fda
Juan Ignacio Polanco <jipolanc@gmail.com>
2022-04-21 16:23:46 +0200

    Remove `@eval` when "requiring" HDF5

--- src/PencilIO/PencilIO.jl
+++ src/PencilIO/PencilIO.jl
@@ -75,7 +75,7 @@ end
 include("mpi_io.jl")
 
 function __init__()
-    @require HDF5="f67ccb44-e63f-5c2f-98bd-6dc0ccc4ba2f" @eval include("hdf5.jl")
+    @require HDF5="f67ccb44-e63f-5c2f-98bd-6dc0ccc4ba2f" include("hdf5.jl")
 end
 
 end
