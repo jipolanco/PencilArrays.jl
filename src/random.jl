fab35e062c76857e6d512b261201a2d43cb7f455
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2024-06-10 08:32:11 +0200

    Really fix tests

--- src/random.jl
+++ src/random.jl
@@ -1,9 +1,14 @@
 using Random
 
-function Random.rand!(rng::AbstractRNG, u::PencilArray, sp::Random.Sampler)
-    rand!(rng, parent(u), sp)
+function Random.rand!(rng::AbstractRNG, u::PencilArray)
+    rand!(rng, parent(u))
     u
 end
+
+# This is to workaround scalar indexing issue with GPUArrays (or at least with JLArrays).
+# GPUArrays.jl defines rand!(::AbstractRNG, ::AnyGPUArray) but not rand!(::AnyGPUArray),
+# which ends up calling a generic rand! implementation in Julia base.
+Random.rand!(u::PencilArray) = rand!(Random.default_rng(), u)
 
 function Random.randn!(rng::AbstractRNG, u::PencilArray, args...)
     randn!(rng, parent(u), args...)
